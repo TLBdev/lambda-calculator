872bd0ae006d8f049003cfb54fd7eaf9c028d2e7
tony <serratedhumor@yahoo.com>
2019-11-06 13:13:07 -0600

    mvp reached

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,13 +1,26 @@
 import React from "react";
+import styled from 'styled-components'
+
 
 export default function NumberButton(props) {
+  if (props.button === '0'){
+    return (
+    <div className = 'zero'>
+      <button >
+        {props.button}
+      </button>
+
+      {/* Display a button element rendering the data being passed down from the parent container on props */}
+    </div>)
+  }
+  
   return (
-    <>
+    <div>
       <button>
         {props.button}
       </button>
 
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+    </div>
+  )
 };
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -5,18 +5,55 @@ import React, {useState} from "react";
  
 import { numbers } from '../../../data' 
 import NumberButton from './NumberButton'
+import styled from 'styled-components'
 
+const StyledNumbers = styled.div`
+      
+      display: flex;
+      max-width: 100%
+      
+      width: 70%;
+      align-items: space-around;
+      justify-content: space-around;
+      flex-wrap: wrap;
+      .zero{
+        width: 62%
+        button{
+          padding: 12%;
+          border-radius: 40%;
+          width: 90% 
+        }
+      }
+      div{
+        width: 30%
+        button{
+          width: 100%;
+          padding: 25%;
+          margin: 10%;
+          background-color: blue;
+          border-radius: 50%; 
+          font-size: 3rem;
+          border-bottom: black
+        }
+      }
+      
+
+`
 //Import your array data to from the provided data file
 
 export default function Numbers() {
   // STEP 2 - add the imported data to state
   const [numArr, changeNumber] = useState(numbers)
   return (
-    <div>
+    <StyledNumbers>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
 it any props needed by the child component*/
-      numArr.map((number) => <NumberButton key = {number} button = {number}/>)}
-    </div>
+      numArr.map((number) => {
+      
+      return <NumberButton key = {number} button = {number}/>
+      
+      })}
+    </StyledNumbers>
   );
 };
